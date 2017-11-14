f933719679521c21c350ac467029bc3ac2f30cb0
samuelparker <samuel.gage.parker@gmail.com>
2017-11-13 20:38:17 -0800

    Create feedrun and ignorelist models

--- feedalerts/n4f2/models.py
+++ feedalerts/n4f2/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Feedruns(models.Model):
+    run_id = models.IntegerField()
+    site_name = models.CharField(max_length=200)
+    feed_profile = models.CharField(max_length=200)
+    feed_name = models.CharField(max_length=200)
+    status_code = models.CharField(max_length=200)
+    status_summary = models.CharField(max_length=200)
+    last_received = models.DateTimeField('date received')
+    last_success = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.run_id
+
+class Ignorelist(models.Model):
+    site_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.site_name
