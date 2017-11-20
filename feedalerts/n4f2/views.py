8e239c1bacc1bcd96a8670e7cb77810c7c72d2f0
samuelparker <samuel.gage.parker@gmail.com>
2017-11-20 15:09:43 -0800

    view to render Feedrun objects at index

--- feedalerts/n4f2/views.py
+++ feedalerts/n4f2/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from n4f2 import utils
+
+from .models import Feedrun, Ignoredsite
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world! You're at the N4F2 index.")
+    feed_run_report = utils.create_feed_run_report()
+    if type(feed_run_report) is str:
+        return HttpResponse(feed_run_report)
+    else:
+        utils.add_feed_runs_to_db(feed_run_report)
+        feedruns = Feedrun.objects.all()
+        context = {
+            'feedruns': feedruns, 
+            'feed_run_report': feed_run_report,
+        }
+        return render(request, 'n4f2/index.html', context)
