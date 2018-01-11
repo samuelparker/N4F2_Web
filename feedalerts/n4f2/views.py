a0ff6c5faa4991a3de1f412ba8f579dca95bc8b9
samuelparker <samuel.gage.parker@gmail.com>
2018-01-11 14:18:10 -0800

    dedicated hl queryset and distinct feedruns on feed profile

--- feedalerts/n4f2/views.py
+++ feedalerts/n4f2/views.py
@@ -12,11 +12,13 @@ def index(request):
         return HttpResponse(feed_run_report)
     else:
         utils.add_feed_runs_to_db(feed_run_report)
-        feedruns = Feedrun.objects.filter(last_received__gte=datetime.now()-timedelta(days=30)).order_by("site_name")
+        feedruns = Feedrun.objects.filter(last_received__gte=datetime.now()-timedelta(days=30)).order_by("site_name").order_by("feed_profile", "-last_received").distinct("feed_profile")
         lateruns = Feedrun.objects.exclude(last_received__gte=datetime.now()-timedelta(days=30)).order_by("site_name")
+        hooklogicruns = Feedrun.objects.filter(site_name="HookLogic", last_received__gte=datetime.now()-timedelta(days=1)).order_by("-last_received")
         context = {
             'feedruns': feedruns, 
             'lateruns': lateruns,
+            'hooklogicruns': hooklogicruns,
             'feed_run_report': feed_run_report,
         }
         return render(request, 'n4f2/index.html', context)
