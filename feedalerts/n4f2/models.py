8740920777259df05437cfaf59205b83e8711086
samuelparker <samuel.gage.parker@gmail.com>
2018-02-19 18:58:31 -0800

    updated models per new schema

--- feedalerts/n4f2/models.py
+++ feedalerts/n4f2/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class FeedProfile(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    watched = models.BooleanField(default=False)
+    last_received = models.DateTimeField('date received')
+    last_success = models.DateTimeField('date published', null=True)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Feedrun(models.Model):
-    run_id = models.IntegerField()
-    site_name = models.CharField(max_length=200)
-    feed_profile = models.CharField(max_length=200)
-    feed_name = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
     status_code = models.CharField(max_length=200)
     status_summary = models.CharField(max_length=200)
     run_link = models.CharField(max_length=200)
     console_link = models.CharField(max_length=200)
     notification_sent = models.BooleanField(default=False)
-    last_received = models.DateTimeField('date received')
-    last_success = models.DateTimeField('date published')
+    feed_profile = models.ForeignKey(FeedProfile, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.run_id)
-
-class Ignoredsite(models.Model):
-    site_name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.site_name
+        return str(self.id)
